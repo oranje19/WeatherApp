828bc9ed657c83cf40d353e7f00e450c564241d3
oranje19 <jasonwintery@gmail.com>
2020-06-16 20:00:42 -0700

    get request

--- website/app.js
+++ website/app.js
@@ -1,7 +1,18 @@
 /* Global Variables */
-let baseURL = `api.openweathermap.org/data/2.5/weather?zip={zip code},us&appid=`
-let apiKey = 'e82d63e77368095deb1786caebcca6dc'
+let baseURL = `api.openweathermap.org/data/2.5/weather?zip=`
+let apiKey = ',us&appid=e82d63e77368095deb1786caebcca6dc'
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+
+const getWeather = async (baseURL, zip, key) => {
+    const res = await fetch (baseURL + zip + key);
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
