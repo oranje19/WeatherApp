a0a9991a4d38ca9d9d37b819f2c0df66c9b28088
oranje19 <jasonwintery@gmail.com>
2020-06-16 19:54:47 -0700

    API credentials

--- website/app.js
+++ website/app.js
@@ -1,5 +1,7 @@
 /* Global Variables */
-
+let baseURL = `api.openweathermap.org/data/2.5/weather?zip={zip code},us&appid=`
+let apiKey = 'e82d63e77368095deb1786caebcca6dc'
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
